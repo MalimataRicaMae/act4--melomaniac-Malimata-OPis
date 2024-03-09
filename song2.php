a083da7b57cc4a8c0c1d591eaf4e5350595b2612
MalimataRicaMae <ricamalimata726@gmail.com>
2024-03-10 05:25:29 +0800

    song2 with lyrics and chords

---
+++ song2.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="favicon.png" type="image/png">
+    <title>song2</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 15px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: lavender;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <pre>
+   <h1 class="song-title"> Isip </h1>
+   <h2 style="font-size:14px;">Song by Healy After Dark</h2>
+   <?php
+   echo"[Intro]";
+
+   echo "<p>Asus2   Dmaj7   F#m7   Dmaj7 </p>";
+   echo "[Verse]";
+   echo "<p>Asus2  </p>";
+   echo "<p>Napanaginipan kita</p>";
+   echo "<p> Dmaj7  </p>";
+   echo "<p>Hawak mo raw ang aking kamay</p>";
+   echo "<p>Asus2  </p>";
+   echo "<p>At ngiti ng mga labi mo</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Ako raw ang dahilan</p>";
+   echo "<br>";
+   echo "<p>Asus2 </p>";
+   echo "<p>At biglang nagising sa katotohanang</p>";
+   echo "<p>Dmaj7  </p>";
+   echo "<p>Wala ka saking tabi't wala 'kong masisi</p>";
+   echo "<p>Asus2 </p>";
+   echo "<p>Ba't pa kailangan pakita sa akin</p>";
+   echo "<p>Dmaj7 </p>";
+   echo "<p>Kung mawawala ka rin sa paggising</p>";
+   echo "[Chorus]";
+   echo "<p>Asus2 </p>";
+   echo "<p>Hindi ko maiwasan na isipin ka</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Tanong sa hangin kung ako ba'y naiisip mo</p>";
+   echo "<p> F#m7 </p>";
+   echo "<p>Litong-lito, litong-lito</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Sa'yo, sa'yo, sa'yo, sa'yo</p>";
+   echo "<br>";
+   echo "<p>Asus2  </p>";
+   echo "<p>Sino ba naman ako para isipin mo</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Hindi mo ba hinihintay ang mga tawag ko</p>";
+   echo "<p> F#m7 </p>";
+   echo "<p>Sa'yo, sa'yo, sa'yo, sa'yo'y</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Litong-lito, litong-lito</p>";
+
+   echo "<p>Asus2   Dmaj7   F#m7   Dmaj7 </p>";
+
+   echo "[Bridge]";
+   echo "<p>F#m7                 E </p>";
+   echo "<p>At sa paglalim ng pagtingin sa'yo</p>";
+   echo "<p>C#m            B </p>";
+   echo "<p>Meron bang pagkakataon ang puso ko</p>";
+   echo "<p>F#m7             E </p>";
+   echo "<p>Hindi alam kung bakit di mabitawan</p>";
+   echo "<p>C#m                  D </p>";
+   echo "<p>Ang kalungkutan na dala mo sa buhay ko</p>";
+  
+   echo "<p>Asus2   Dmaj7   F#m7   Dmaj7 </p>";
+
+   echo "[Chorus]";
+   echo "<p>Asus2 </p>";
+   echo "<p>Hindi ko maiwasan na isipin ka</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Tanong sa hangin kung ako ba'y naiisip mo</p>";
+   echo "<p> F#m7 </p>";
+   echo "<p>Litong-lito, litong-lito</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Sa'yo, sa'yo, sa'yo, sa'yo</p>";
+   echo "<br>";
+   echo "<p>Asus2  </p>";
+   echo "<p>Sino ba naman ako para isipin mo</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Hindi mo ba hinihintay ang mga tawag ko</p>";
+   echo "<p> F#m7 </p>";
+   echo "<p>Sa'yo, sa'yo, sa'yo, sa'yo'y</p>";
+   echo "<p> Dmaj7 </p>";
+   echo "<p>Litong-lito, litong-lito</p>";
+
+   echo "<p>Asus2   Dmaj7   F#m7   Dmaj7 </p>";
+   ?>
+   <a href="index.php">Go Back</a>
+   </body>
+</html>
