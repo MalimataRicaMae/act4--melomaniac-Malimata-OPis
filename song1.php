af7c950b3506720d91207f155c5370049638503b
MalimataRicaMae <ricamalimata726@gmail.com>
2024-03-10 05:24:51 +0800

    song1 with lyrics and chords

---
+++ song1.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="favicon.png" type="image/png">
+    <title>song1</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 15px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: left;
+            font-family: Arial, sans-serif;
+            background-color: lavender ;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+
+    </style>
+</head>
+<body>
+   <pre> 
+   <h1 class="song-title"> Illicit Affairs </h1>
+   <h2 style="font-size:14px;">song by Taylor Swift</h2>
+   <?php
+   echo "[Intro]";
+
+   echo "<p> A  F#m  D  E";
+
+   echo "[Verse 1]";
+
+   echo "<p>A                          F#m</p>";
+   echo "<p>Make sure nobody sees you leave</p>";
+   echo "<p>                                     D</p>";
+   echo "<p>Hood over your head, keep your eyes down</p>";
+   echo "<p>                                   C#m</p>";
+   echo "<p>Tell your friends you're out for a run</p>";
+   echo "<p>           E </p>";
+   echo "<p>You'll be flushed when you return</p>";
+   echo "<p> A                          F#m </p>";
+   echo "<p>Take the road less traveled by</p>";
+   echo "<p>                              D </p>";
+   echo "<p>Tell yourself you can always stop</p>";
+   echo "<p>                            C# </p>";
+   echo "<p>What started in beautiful rooms</p>";
+   echo "<p>          E </p>";
+   echo "<p>Ends with meetings in parking lots</p>";
+   
+   echo "[Chorus]";
+
+   echo "<p>C#                           D </p>";
+   echo "<p>And that's the thing about illicit affairs</p>";
+   echo "<p>  Esus4                        E </p>";
+   echo "<p>And clandestine meetings and stolen stares</p>";
+   echo "<p>C#m                    D </p>";
+   echo "<p>They show their truth one single time</p>";
+   echo "<p> Esus4                             E </p>";
+   echo "<p>But they lie and they lie and they lie</p>";
+   echo "<p> A ";
+   echo "<p>A million little times</p>";
+
+   echo "[Bridge]";
+
+   echo "<p>                   A </p>";
+   echo "<p>And you wanna scream</p>";
+   echo "<p>A </p>" ;
+   echo "<p>Don't call me kid, don't call me baby</p>";
+   echo "<p>F#m </p>";
+   echo "<p>Look at this godforsaken mess that you made me</p>";
+   echo "<p> D                        C#m  </p>                E ";
+   echo "<p>You showed me colors you know I can't see with anyone else</p>";
+   echo "<p>A </p>";
+   echo "<p>Don't call me kid, don't call me baby</p>";
+   echo "<p>   F#m       </p>  ";
+   echo "<p>Look at this idiotic fool that you made me</p>";
+   echo "<p>D                       C#m    </p>";
+   echo "<p>You taught me a secret language</p>";
+   echo "<p>                    E </p>";
+   echo "<p>I can't speak with anyone else</p>";
+
+   echo "[Outro]";
+
+   echo "<br>";
+   echo "<p>         F#m  E   D </p>";
+   echo "<p>And you know damn well</p>";
+   echo "<p>                   E </p>";
+   echo "<p>For you, I would ruin myself</p>";
+   echo "<p> A </p>";
+   echo "<p>A million little times</p>";
+   ?>
+   <br>
+   <a href="index.php">Go Back</a>
+   </body>
+</html>
