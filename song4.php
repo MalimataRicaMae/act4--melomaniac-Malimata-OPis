c5701491cfaeb1fe0d526d50ccc340dd5b825532
MalimataRicaMae <ricamalimata726@gmail.com>
2024-03-10 05:27:30 +0800

    song4 with lyrics and chords

---
+++ song4.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="favicon.png" type="image/png">
+    <title>song4</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 15px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: lavender;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<pre>
+    <h1 class="song-title"> Yellow </h1>
+   <h2 style="font-size:14px;">song by Coldplay</h2>
+   <?php
+   echo "[Intro]";
+   echo "<p>G   G 
+G   D   C   G</p>";
+   echo "[Verse 1]";
+   echo "<p>G  </p>"; 
+   echo "<p> Look at the stars</p>";
+   echo "<p>                       D </p>";
+   echo "<p>Look how they shine for you</p>";
+   echo "<p>                   Cmaj7 </p>";
+   echo "<p>And everything you do</p>";
+   echo "<p>Yeah, they were all yellow</p>";
+   echo "<p>G </p>";
+   echo "<p>I came along</p>";
+   echo "<p>                   D</p>";
+   echo "<p>I wrote a song for you</p>";
+   echo "<p>                        Cmaj7 </p>";
+   echo "<p>And all the things you do</p>";
+   echo "<p>And it was called Yellow</p>"; 
+   echo "<p>G                D</p>"; 
+   echo "<p>So then I took my turn</p>";
+   echo "<p>                             Cmaj7 </p>";
+   echo "<p>Oh, what a thing to have done</p>";
+   echo "<p>                    G   Gsus4</p>";
+   echo "<p>And it was all yellow</p>";
+   echo "[Chorus 1]";
+   echo "<p>Cmaj7      Em7           D</p>";
+   echo "<p>Your skin, oh yeah, your skin and bones</p>";
+   echo "<p>Cmaj7  Em7           D       </p>";
+   echo "<p>Turn into something beautiful</p>";
+   echo "<p>    Cmaj7     Em7         D         Cmaj7</p>";
+   echo "<p>And you know, you know I love you so</p>";
+   echo "<p>You know I love you so</p>";  
+   echo "[Instrumental]";
+   echo "<p> G   D   C   g   Gsus4 </p>";
+   echo " [Verse 2]";
+   echo "<p>G </p>";
+   echo "<p>I swam across</p>";
+   echo "<p>                    D </p>";
+   echo "<p>I jumped across for you</p>";
+   echo "<p>                      Cmaj7 </p>";
+   echo "<p>Oh, what a thing to do</p>";
+   echo "<p>'Cause you were all yellow</p>";
+   echo "<p>G </p>";
+   echo "<p>I drew a line</p>";
+   echo "<p>                 D </p>";
+   echo "<p>I drew a line for you</p>";
+   echo "<p>                      Cmajy </p>";
+   echo "<p>Oh, what a thing to do</p>";
+   echo "<p>                    G   Gsus4 </p>";
+   echo "<p>And it was all yellow</p>";
+   echo "[Chorus 2]";
+   echo "<p>C              Em7 </p>";
+   echo "<p>And your skin, oh yeah, your skin and bones</p>"; 
+   echo "<p>Cmaj7  Em7          D </p>";
+   echo "<p>Turn into something beautiful</p>"; 
+   echo "<p>Cmaj7                      D             Cmaj7 </p>";
+   echo "<p>And you know, for you, I'd bleed myself dry</p>";
+   echo "<p>For you, I'd bleed myself dry</p>";
+   echo "[Instrumental]";
+   echo "<p> G   D   C   g   Gsus4 </p>";
+   echo "[Bridge]";
+   echo "<p> G   D   </p>";
+   echo "<p>It's true</p>";
+   echo "<p>                         D</p>";
+   echo "<p>Look how they shine for you</p>";
+   echo "<p>                        Cmaj7    </p>";
+   echo "<p>Look how they shine for you</p>";
+   echo "<p>                       G </p>";
+   echo "<p>Look how they shine for</p>";
+   echo "<p>                         D   </p>";
+   echo "<p>Look how they shine for you</p>";
+   echo "<p>                        Cmaj7   </p>";
+   echo "<p>Look how they shine for you</p>";
+   echo "<p>Look how they shine</p>";
+   echo "[Outro]";
+   echo "<p> G    </p>";
+   echo "<p>Look at the stars</p>";
+   echo "<p>                        Dm7   </p>";
+   echo "<p>Look how they shine for you</p>";
+   echo "<p>                           Cmaj7  </p>";
+   echo "<p>And all the things that you do</p>";
+   ?>
+   <a href="index.php">Go Back</a>
+   </body>
+</html>
