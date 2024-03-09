88462b054b46b44185a98464a37144a4f8887162
MalimataRicaMae <ricamalimata726@gmail.com>
2024-03-10 05:26:07 +0800

    song3 with lyrics and chords

---
+++ song3.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="favicon.png" type="image/png">
+    <title>song3</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 15px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: lavender;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <pre>
+    <h1 class="song-title"> Drunk Text </h1>
+   <h2 style="font-size:14px;">song by Henry Moodie</h2>
+   <?php
+  echo "[Verse 1]";
+  echo "<p>       C/G </p>";
+  echo "5th of November</p>";
+  echo "<p>                  Em  </p>";
+  echo "<p>When I walked you home</p>";
+  echo "<p>           Am  </p>";
+  echo "<p>That's when I nearly said it</p>";
+  echo "<p>                              F  </p>";
+  echo "<p>But then said 'Forget it' and froze</p>";
+  echo "<br>";
+  echo "<p>         C/G </p>";
+  echo "<p>Do you remember?</p>";
+  echo "<p>              Em</p>";
+  echo "<p>You probably don't</p>";
+  echo "<p>             Am </p>";
+  echo "<p>'Cause the sparks in the sky</p>";
+  echo "<p>                                  F     G </p>";
+  echo "<p>Took a hold of your eyes while we spoke</p>";
+  echo "[Pre-Chorus]";
+  echo "<p>C </p>";
+  echo "<p>Yesterday, drank way too much</p>";
+  echo "<p>           Am </p>";
+  echo "<p>And stayed up too late</p>";
+  echo "<p>                        Dm </p>";
+  echo "<p>Started to write but I wanna say</p>";
+  echo "<p>Dm7                                            G* </p>";
+  echo "<p>Deleted the message, but I still remember it said</p>";
+  echo "Chorus";
+  echo "<p> C </p>";
+  echo "<p>I wish I was who you drunk texted at midnight</p>";
+  echo "<p> Em                       Dm </p>";
+  echo "<p>Wish I was the reason you stay up 'til 3</p>";
+  echo "<p>And you can't fall asleep</p>";
+  echo "<p>F                   G7 </p>";
+  echo "<p>Waiting for me to reply</p>";
+  echo "<br>";
+  echo "<p>C                                       Em</p>";       
+  echo "<p>I wish I was more than just someone you walk by</p>";
+  echo "<p>                           Dm </p>";
+  echo "<p>Wish I wasn't scared to be honest and open</p>";
+  echo "<p>Instead of just hoping</p>";
+  echo "<p>       F                      Fm</p>";
+  echo "<p>You'd feel what I'm feeling inside</p>";
+  echo "[Verse 2]";
+  echo "<p>          C/G </p>";
+  echo "<p>April the 7th</p>";
+  echo "<p>                 Em</p>";
+  echo "<p>And nothing has changed</p>";
+  echo "<p>      Am </p>";
+  echo "<p>It's hard to get by</p>";
+  echo "<p>                                  F </p>";
+  echo "<p>When you're still on my mind every day</p>";
+  echo "<br>";
+  echo "<p>            C/G </p>";
+  echo "<p>Sometimes I question</p>";
+  echo "<p>                Em </p>";
+  echo "<p>If you feel the same?</p>";
+  echo "<p>       Am </p>";
+  echo "<p>Do we make stupid jokes?</p>";
+  echo "<p>                                    F         G</p>";
+  echo "<p>Trying to hide that we're both too afraid to say</p>";
+  echo "Chorus";
+  echo "<p> C </p>";
+  echo "<p>I wish I was who you drunk texted at midnight</p>";
+  echo "<p> Em                       Dm </p>";
+  echo "<p>Wish I was the reason you stay up 'til 3</p>";
+  echo "<p>And you can't fall asleep</p>";
+  echo "<p>F                   G7 </p>";
+  echo "<p>Waiting for me to reply</p>";
+  echo "<br>";
+  echo "<p>C                                       Em</p>";       
+  echo "<p>I wish I was more than just someone you walk by</p>";
+  echo "<p>                           Dm </p>";
+  echo "<p>Wish I wasn't scared to be honest and open</p>";
+  echo "<p>Instead of just hoping</p>";
+  echo "<p>       F                      Fm</p>";
+  echo "<p>You'd feel what I'm feeling inside</p>";
+  echo "[Bridge]";
+  echo "Chorus";
+  echo "<p> C </p>";
+  echo "<p>I wish I was who you drunk texted at midnight</p>";
+  echo "<p> Em                       Dm </p>";
+  echo "<p>Wish I was the reason you stay up 'til 3</p>";
+  echo "<p>And you can't fall asleep</p>";
+  echo "<p>F                   G7 </p>";
+  echo "<p>Waiting for me to reply</p>";
+  echo "<br>";
+  echo "<p>C                                       Em</p>";       
+  echo "<p>I wish I was more than just someone you walk by</p>";
+  echo "<p>                           Dm </p>";
+  echo "<p>Wish I wasn't scared to be honest and open</p>";
+  echo "<p>Instead of just hoping</p>";
+  echo "<p>       F                      Fm</p>";
+  echo "<p>You'd feel what I'm feeling inside</p>";
+  echo "Chorus";
+  echo "<p> C </p>";
+  echo "<p>I wish I was who you drunk texted at midnight</p>";
+  echo "<p> Em                       Dm </p>";
+  echo "<p>Wish I was the reason you stay up 'til 3</p>";
+  echo "<p>And you can't fall asleep</p>";
+  echo "<p>F                   G7 </p>";
+  echo "<p>Waiting for me to reply</p>";
+  echo "<br>";
+  echo "<p>C                                       Em</p>";       
+  echo "<p>I wish I was more than just someone you walk by</p>";
+  echo "<p>                           Dm </p>";
+  echo "<p>Wish I wasn't scared to be honest and open</p>";
+  echo "<p>Instead of just hoping</p>";
+  echo "<p>       F                      Fm</p>";
+  echo "<p>You'd feel what I'm feeling inside</p>";
+  echo "<p>Dm</p>";
+  echo "<p>Oh, and here we go again</p>";
+  echo "<p> G7 </p>";
+  echo "<p>Destroy myself to keep my friend</p>";
+  echo "<p> Em                                        F</p>";
+  echo "<p>Hiding away 'cause I was afraid you'd say no</p>";
+  echo "<p> Dm </p>";
+  echo "<p>I wonder if I cross your mind</p>";
+  echo "<br>";
+  echo "<p> G</p>";
+  echo "<p>Half as much as you do mine</p>";
+  echo "<p>      C</p>";
+  echo "<p>If I tell you the truth</p>";
+  echo "<p> Em </p>";
+  echo "<p>What will I lose?</p>";
+  echo "<p>         F    Fm</p>";
+  echo "<p>I don't know</p>";
+  echo "[Chorus/Ending]";
+  echo "<p> C                                          Em</p>";
+  echo "<p>I wish I'd sent you that drunk text that midnight</p>";
+  echo "<p>                            Dm </p>";
+  echo "<p>I was just scared it would ruin our friendship</p>";
+  echo "<p>But I really meant it</p>";
+  echo "<p>  F                      Fm </p>";
+  echo "<p>I wonder how you would reply</p>";
+   ?>
+   <a href="index.php">Go Back</a>
+   </body>
+</html>
