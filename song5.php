a0dbae9a8e954d0fe0cb25c298443bb60297f074
MalimataRicaMae <ricamalimata726@gmail.com>
2024-03-10 05:28:08 +0800

    song5 with lyrics and chords

---
+++ song5.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="favicon.png" type="image/png">
+    <title>song5</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 15px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: lavender;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<pre>
+    <h1 class="song-title"> My Love Mine All Mine </h1>
+   <h2 style="font-size:14px;">song by Mitski</h2>
+  <?php
+    echo "[Intro]";
+    echo "<p> Amaj7   Dm6   </p>";
+    echo "<br>";
+    echo "<p> Amaj7     </p>";
+    echo "<p>Moon, a hole of light</p>";
+    echo "<p> C#7 </p>";
+    echo "<p>Through the big top tent up high</p>";
+    echo "<p>D         </p>";
+    echo "<p>Here before and after me</p>";
+    echo "<p> Dm7   </p>";
+    echo "<p>Shinin' down on me</p>";
+    echo "<br>";
+    echo "<p>Amaj7   </p>";
+    echo "<p>Moon, tell me if I could</p>";
+    echo "<p> C#7  </p>";
+    echo "<p>Send up my heart to you?</p>";
+    echo "<p> D   </p>";
+    echo "<p>So, when I die, which I must do</p>";
+    echo "<p>         Dm   </p>";
+    echo "<p>Could it shine down here with you?</p>";
+    echo "[Chorus]";
+    echo "<p>       Amaj7      C#7  </p>";
+    echo "<p>'Cause my love is mine, all mine</p>";
+    echo "<p>D       Dm   </p>";
+    echo "<p>I love, my, my, mine</p>";
+    echo "<p>Amaj7                  C#7</p>";
+    echo "<p>Nothing in the world belongs to me</p>";
+    echo "<p> D           Dm   </p>";
+    echo "<p>But my love, mine, all mine, all mine</p>";
+    echo "[Post-Chorus]";
+    echo "<p> Amaj7   C#7   D   Dm </p>";
+    echo "[Verse 2]";
+    echo "<p>Amaj7   </p>";
+    echo "<p>My baby here on earth</p>";
+    echo "<p> C#7 </p>";
+    echo "<p>Showed me what my heart was worth</p>";
+    echo "<p> D    </p>";
+    echo "<p>So, when it comes to be my turn</p>";
+    echo "<p>           Dm   </p>";
+    echo "<p>Could you shine it down here for her?</p>";
+    echo "[Chorus]";
+    echo "<p>       Amaj7      C#7  </p>";
+    echo "<p>'Cause my love is mine, all mine</p>";
+    echo "<p>D       Dm   </p>";
+    echo "<p>I love, my, my, mine</p>";
+    echo "<p>Amaj7                  C#7</p>";
+    echo "<p>Nothing in the world belongs to me</p>";
+    echo "<p> D           Dm   </p>";
+    echo "<p>But my love, mine, all mine, all mine</p>";
+    echo "<p>Amaj7                C#7</p>";
+    echo "<p>Nothing in the world is mine for free</p>";
+    echo "<p> D           Dm   </p>";
+    echo "<p>But my love, mine, all mine, all mine</p>";
+    
+    ?>
+    <a href="index.php">Go Back</a>
+   </body>
+</html>
